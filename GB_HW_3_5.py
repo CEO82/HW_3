e3608cdcec229099c9c245438bfcb21e424564eb
CEO82 <git-test@test.com>
2025-08-18 21:51:57 +0100

    10 - th commit, HW_3_5 was made in wrong way and I decided to start from scratch in new file HW_3_5.2 - was created

--- GB_HW_3_5.py
+++ GB_HW_3_5.py
@@ -10,14 +10,30 @@ Enter должна выводиться сумма чисел. Пользова�
 if __name__ != '__main__':
     print(f'\nThis file is not for import!!!')
 
-def sum_func(lst_for_sum: str)-> float:
-    return sum(lst_for_sum)
+'''def sum_func(lst_for_sum: str)-> float:
+    return sum(lst_for_sum)'''
 
 def make_list_of_floats(user_list: list)-> list:
     '''
     This function should check that list contain any number/s if it does not, return marker 'no digits', if it contain digits return list only with numbers.
     '''
+    print(user_list)
     num_list = [n for n in user_list if isinstance(n, (int, float))]
+    print(num_list)
+    num_list = []
+
+    for n in user_list:
+        try:
+            float(n)
+            num_list.append(float(n))
+
+        except ValueError:
+            num_list = num_list
+
+    print(num_list)
+
+
+
     if num_list == []:
         return ['no digits']
     else:
@@ -38,15 +54,31 @@ def get_string()-> list:
     user_str = input(f''
                      f'\nPlease enter only numbers separated by spaces,'
                      f'\nIf you want finish the program please enter only symbol ~ '
-                     f'\nIf you will input numbers and at the end you will put symbol ~ , program will do last calculation and be finished after it')
+                     f'\nIf you will input numbers and at the end you will put symbol ~ , \nprogram will do last calculation and be finished after it:-> ')
     return user_str.split()
 
 def main_func():
-    
-    pass
+    '''
+
+    :return:
+    '''
+    summ = 0
+    input_list = get_string()
+    stop_check = go_stop_check(input_list)
+    if stop_check == 'hard_stop':
+        return print(f'\nProgram finished, the sum is {summ}')
+    elif stop_check == 'sum_and_stop':
+        summ = summ + sum(make_list_of_floats(input_list))
+        return print(f'\nProgram finished, the sum is {summ}')
+    else:
+        print(make_list_of_floats(input_list))
+        summ = summ + sum(make_list_of_floats(input_list))
+        print(f'\nCurrent summ is {summ}')
+        get_string()
+
 
 
 if __name__ == '__main__':
 
-    print(main_func())
+    main_func()
 
