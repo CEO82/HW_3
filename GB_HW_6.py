fdc3b6dafd913ddb4667077bf7202c0998154b81
CEO82 <git-test@test.com>
2025-08-25 12:52:06 +0100

    17 - th commit, HW_3_6 next step...

--- GB_HW_6.py
+++ GB_HW_6.py
@@ -2,3 +2,15 @@
 возвращающую их же, но с прописной первой буквой. Например, print(int_func(‘text’)) -> Text.
 '''
 
+import re
+
+def int_func():
+    '''description'''
+    new_str = take_input()
+    return new_str.title()
+
+
+
+def take_input():
+    '''description'''
+
