59dc412e0f61a961d4a566402330268965e625b0
CEO82 <git-test@test.com>
2025-09-09 22:06:47 +0100

    33 - th commit, GB_GPT_HW_2.0. - continue

--- GB_GPT_HW_2.0.py
+++ GB_GPT_HW_2.0.py
@@ -40,8 +40,9 @@ import re
 
 
 def operations(n1, op, n2):
+    ''' This function receives  three arguments firs number - n1, operation - op, second number - n2. And will return result of operation. If any errors appear it will show message with original string and message that user entered something wrong and send user to repeat input '''
 
-
+    print(f'{n1}  {op}  {n2}')
     operator_list = {
         '+': operator.add,
         '-': operator.sub,
@@ -50,39 +51,47 @@ def operations(n1, op, n2):
         '**': operator.pow,
     }
 
+    return operator_list[op](n1, n2)
+
     pass
 
 
 
 
-test_string = '4 + 3*2 - 5 + 20 * 22 / 5 ** 2 - 2 ** 0.5'
+test_string = '4 + 3 * 2 - 5 + 20 * 22 / 5 ** 2 - 2 ** 0.5'
 
 test_list = test_string.split()
 
-''' ['4', '+', '3*2', '-', '5', '+', '20', '*', '22', '/', '5', '**', '2', '-', '2', '**', '0.5'] '''
+''' ['4', '+', '3 * 2', '-', '5', '+', '20', '*', '22', '/', '5', '**', '2', '-', '2', '**', '0.5'] '''
 
 print(test_list)
 
-while True: #For / operation!
+operators_list = ['**', '*', '/', '+', '-']
 
-    if test_list.count('/') > 0:
-        ind_n1 = test_list.index('/') - 1
-        ind_op = test_list.index('/')
-        ind_n2 = test_list.index('/') + 1
+for oper_symb in operators_list:
+    while True:  # For / operation!
 
-        n1 = test_list[ind_n1]
-        op = test_list[ind_op]
-        n2 = test_list[ind_n2]
+        if test_list.count(oper_symb) > 0:
+            ind_n1 = test_list.index(oper_symb) - 1
+            ind_op = test_list.index(oper_symb)
+            ind_n2 = test_list.index(oper_symb) + 1
 
-        result_numb = operations(n1, op, n2)
+            n1 = float(test_list[ind_n1])
+            op = test_list[ind_op]
+            n2 = float(test_list[ind_n2])
 
-        test_list.pop(ind_n1)
-        test_list.pop(ind_n1)
-        test_list.pop(ind_n1)
+            result_numb = operations(n1, op, n2)
 
-        test_list.insert(ind_n1, result_numb)
+            test_list.pop(ind_n1)
+            test_list.pop(ind_n1)
+            test_list.pop(ind_n1)
 
-        continue
+            test_list.insert(ind_n1, result_numb)
 
-    else:
-        break
+            continue
+
+        else:
+            print(test_list)
+            break
+
+
